a26b70625eb1162c2484ab31e8e1647d38f1e753
Najim <najim.sutar@gmail.com>
2020-05-23 13:26:23 -0700

    Update FlattenJson.java
    Parsed nested JSONObject and flatten them to <parent-key>.<current-key> format.

--- FlattenJson.java
+++ FlattenJson.java
@@ -10,24 +10,31 @@ public class FlattenJson {
 			+ "        	\"h\": false,\n" + "        }\n" + "    }\n" + "}";
    
 	/**
-   * Method to read json as JsonObject and flatten it into Map.
-   * 
-   * @param jsonObject - JsonObject after parsing JSON string to an object using json-simple-1.1.1.jar
-   * @param jsonMap - Out parameter to store falttened JSON as key-value pair
-   * @param parentKey - Parent JsonObject-key is passed, in case of nested JsonObject 
-   */
+	* Method to read json as JsonObject and flatten it into Map.
+	*
+	* @param jsonObject - JsonObject after parsing JSON string to an object using json-simple-1.1.1.jar
+	* @param jsonMap - Out parameter to store falttened JSON as key-value pair
+	* @param parentKey - Parent JsonObject-key is passed, in case of nested JsonObject 
+	*/
 	public static void populateJsonMap(JSONObject jsonObject, LinkedHashMap<String, String> jsonMap, String parentKey) {
 		for (Object key : jsonObject.keySet()) {
 			String strKey = (String) key;
-			Object keyvalue = jsonObject.get(strKey);
-	    jsonMap.put(strKey, String.valueOf(keyvalue));
+			String orgkey = strKey;
+			if (parentKey != null) {
+				strKey = parentKey + "." + strKey;
+			}
+			Object keyvalue = jsonObject.get(orgkey);
+			if (keyvalue instanceof JSONObject) {
+				populateJsonMap((JSONObject) keyvalue, jsonMap, strKey);
+			} else {
+				jsonMap.put(strKey, String.valueOf(keyvalue));
+			}
 		}
 	}
 
 	public static void main(String[] args) {
 		LinkedHashMap<String, String> jsonMap = new LinkedHashMap<String, String>();
-		
-    // Parsing JSON from string to JSONObject using json-simple-1.1.1.jar
+		// Parsing JSON from string to JSONObject using json-simple-1.1.1.jar
 		JSONParser parser = new JSONParser();
 		Object jsonObj = null;
 		try {
@@ -41,8 +48,6 @@ public class FlattenJson {
 			System.out.println("Exception: " + ex.getMessage());
 			System.out.println(ex);
 		}
-
 	}
-
 }
 
